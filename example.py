59d1945f6e840fba3600fd8ab4a52a52827ab450
Karst3nz <gruk5200@gmail.com>
2025-05-24 00:38:01 +0300

    example includes how to handle exceptions

--- example.py
+++ example.py
@@ -1,5 +1,6 @@
 from cloudflare import CloudflareAsyncAPI
 import asyncio
+from log import create_logger, logging
 
 async def example():
     cf = CloudflareAsyncAPI.from_api_token( # Requires a Full DNS and USER permissions. Better use ".from_global_key()" is more simple
@@ -10,24 +11,27 @@ async def example():
         email="YOUR-CF-EMAIL",
         api_key="YOUR-CF-GLOBAL-KEY"
     )
-    async with cf:
-        # Register the domain (creates a new zone)
-        zone_id, ns1, ns2 = await cf.register_domain("example.com")
-        cf._logger.info("Zone ID: {}".format(zone_id))
-        cf._logger.info("NS1: {}".format(ns1))
-        cf._logger.info("NS2: {}".format(ns2))
-        # Add an A record
-        await cf.add_dns_record(
-            zone_id=zone_id,
-            record_type="A",
-            name="www",
-            content="192.0.2.2",
-            proxied=True  # Enable Cloudflare proxy
-        )
+    logger = create_logger("example", prefix="CF-Example", level=logging.DEBUG)
+    try:
+        async with cf:
+            # Register the domain (creates a new zone)
+            zone_id, ns1, ns2 = await cf.register_domain("example.com")
+            logger.info("Zone ID: {}".format(zone_id))
+            logger.info("NS1: {}".format(ns1))
+            logger.info("NS2: {}".format(ns2))
+            # Add an A record
+            await cf.add_dns_record(
+                zone_id=zone_id,
+                record_type="A",
+                name="www",
+                content="192.0.2.2",
+                proxied=True  # Enable Cloudflare proxy
+            )
 
-        # Optionally wait until the zone is active
-        await cf.wait_until_active(zone_id)        
-
-
+            # Optionally wait until the zone is active
+            await cf.wait_until_active(zone_id)    
+    except cf.UserCredsInvalid as e:
+        logger.critical(e)
+        
 if __name__ == "__main__":
     asyncio.run(example())
